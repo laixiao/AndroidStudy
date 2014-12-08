8973c007c9ed9dbe60b9cba0b3fceadaae8c9636
laixiao <2515097216@qq.com>
2014-12-08 14:21:32 +0800

    说说界面

---
+++ src/com/study/main/ui/User/otherInfo.java
@@ -0,0 +1,24 @@
+package com.study.main.ui.User;
+ 
+import com.study.main.R;
+import com.study.main.Entity.QiangYu;
+import com.study.main.Entity.User;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class otherInfo extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.otheractivity);
+		
+		User user=(User) getIntent().getSerializableExtra("data");
+		Toast.makeText(otherInfo.this,user.toString(), Toast.LENGTH_LONG).show();
+		
+	}
+
+}
