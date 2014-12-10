c4f7b59f731609bb8c0ddef93b77ddbe01dd9d32
laixiao <2515097216@qq.com>
2014-12-10 23:45:01 +0800

    版本分支

---
+++ src/com/study/main/Entity/Favour.java
@@ -0,0 +1,29 @@
+package com.study.main.Entity;
+
+import cn.bmob.v3.BmobObject;
+
+public class Favour extends BmobObject{
+
+	private ShuoShuo shuoshuo;
+	private String userId;
+	
+	public ShuoShuo getShuoshuo() {
+		return shuoshuo;
+	}
+	public void setShuoshuo(ShuoShuo shuoshuo) {
+		this.shuoshuo = shuoshuo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+
+
+
+	
+	
+	
+}
